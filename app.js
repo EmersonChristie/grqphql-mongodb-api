da8afe914813423d3364843e3d3fb05766062b96
Emerson Christie <emersonchristie@Emersons-iMac.local>
2019-08-19 09:43:04 -0700

    graphQL createArt event

--- app.js
+++ app.js
@@ -7,18 +7,91 @@ var db = require("./artwork.json");
 
 const app = express();
 
+const artwork = [];
+
 app.use(bodyParser.json());
 
 app.use(
   "/graphql",
   graphqlHttp({
     schema: buildSchema(`
+        type Artist {
+          _id: ID!
+          artCldArtistId: ID
+          name: String!
+          about: String
+          image: String
+        }
+
+        type Image {
+          _id: ID!
+          artCldImgId: ID
+          url: String!
+          smallUrl: String
+          mediumUrl: String
+          largeUrl: String
+          giantUrl: String
+          primaryR: Int
+          primaryG: Int
+          primaryB: Int
+          secondaryR: Int
+          secondaryG: Int
+          secondaryB: Int
+          tertiaryR: Int
+          tertiaryG: Int
+          tertiaryB: Int
+          width: Int
+          height: Int
+        }
+
+        type Dimensions {
+          formatted: String!
+          height: Float!
+          width: Float!
+          depth: Float
+        }
+
+        type Art {
+          _id: ID!
+          artCldId: ID
+          artist: Artist!
+          date: String!
+          _price: Float
+          _formattedPrice: String
+          dimensions: Dimensions!
+          image: Image!
+          title: String!
+          tags: [String!]
+        }
+
+        input ArtInput {
+          artist: Artist!
+          date: String!
+          dimensions: Dimensions!
+          image: Image!
+          title: String!
+          tags: [String!]
+        }
+
+        type Favorite {
+            _id: ID!
+            art: Art!
+            dateCreated: String!
+        }
+
+        input FavoriteInput {
+            art: Art!
+            dateCreated: String!
+        }
+
         type RootQuery {
-            favorites: [String!]!
+          favorites: [String!]!
+          artWorks: [Art!]!
         }
 
         type RootMutation {
-            createFavorite(name: String): String
+          createArt(artInput: ArtInput): Art
+          createFavorite(name: String): String
         }
 
         schema {
@@ -27,11 +100,25 @@ app.use(
         }
     `),
     rootValue: {
+        artWorks: () => {
+
+        }
+        createArt: args => {
+            const art = new Art({
+                artist: args.artInput.artist,
+                date: args.artInput.date,
+                dimensions: args.artInput.dimensions,
+                image: args.artInput.image,
+                title: args.artInput.title,
+                tags: args.artInput.tags
+            });
+            
+        }
       favorites: () => {
         return ["lskdjflksdjf", "slkdjflksdjf", "sldkjflsakjdf"];
       },
       createFavorite: args => {
-        const favoriteName = args.name;
+        _id: Math.random().toString();
         return favoriteName;
       }
     },
